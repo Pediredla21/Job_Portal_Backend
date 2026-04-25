1e09ac77a3c8677446f4a57aca51880849d54648
SriTanyaG <gsritanya@gmail.com>
2026-04-25 21:28:38 +0530

    test 2

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -10,7 +10,7 @@ export const useAuth = () => {
   }
   return context
 }
-// test merge conflict
+// test merge conflict test
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
   const [loading, setLoading] = useState(true)
